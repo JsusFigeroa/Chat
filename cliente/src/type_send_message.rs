316a7bdf54af5c6892d70606de7926f2a2da06f8
Jesus Figueroa <jsusfigueroa@ciencias.unam.mx>
2026-03-19 23:09:13 -0600

    Se terminan de implementar las enumeraciones para los mensajes que se pueden recibir y enviar

--- cliente/src/type_send_message.rs
+++ cliente/src/type_send_message.rs
@@ -11,20 +11,46 @@ pub(crate)  enum TypeSendMessage {
     },
     #[serde(rename = "STATUS")]
     Status {
-        status: Status,
+        status: Status
     },
     #[serde(rename = "USERS")]
     Users,
     #[serde(rename = "TEXT")]
     Text {
         username: String,
-        text: String
+        text: String,
     },
     #[serde(rename = "PUBLIC_TEXT")]
     PublicText {
+        text: String,
+    },
+    #[serde(rename = "NEW_ROOM")]
+    NewRoom {
+        roomname: String
+    },
+    #[serde(rename = "INVITE")]
+    Invite {
+        roomname: String,
+        usernames: Vec<String>
+    },
+    #[serde(rename = "JOIN_ROOM")]
+    JoinRoom {
+        roomname: String
+    },
+    #[serde(rename = "ROOM_USERS")]
+    RoomUsers {
+        roomname: String
+    },
+    #[serde(rename = "ROOM_TEXT")]
+    RoomText {
+        roomname: String,
         text: String
     },
+    #[serde(rename = "LEAVE_ROOM")]
+    LeaveRoom {
+        roomname: String
+    },
     #[serde(rename = "DISCONNECT")]
-    DISCONNECT
+    Disconect,
 }
 
